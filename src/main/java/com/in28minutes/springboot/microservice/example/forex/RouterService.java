074d1fc6a1c7e096966fe1625fe551e9c8d0d28c
nmudrinic <nenad.mudrinic@devoteam.com>
2019-02-26 10:17:22 +0100

    c1

---
+++ src/main/java/com/in28minutes/springboot/microservice/example/forex/RouterService.java
@@ -0,0 +1,52 @@
+package com.in28minutes.springboot.microservice.example.forex;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+
+
+
+@RestController
+public class RouterService {
+	
+	 // @Autowired
+	 // private CurrencyExchangeServiceProxy proxy;
+	
+	  @CrossOrigin(origins = "http://localhost:8002")
+	  @GetMapping("/getExchangeRates")
+	  public Object[] retrieveExchangeValue(){
+	    
+		 //proxy.fetchList(); 
+        //List<ExchangeValue> response = proxy.getList();	
+		  
+		  System.out.println("HIIIIT");
+		  
+		/*  ResponseEntity<List<ExchangeValue>> responseEntity = new RestTemplate().exchange(
+			        "http://localhost:8000/currency-exchange/getList",  HttpMethod.GET, null, new ParameterizedTypeReference<List<ExchangeValue>>() {
+		            });*/
+		  
+		  
+			  ResponseEntity<Object[]> responseEntity = new RestTemplate().exchange(
+	        "http://localhost:8000/currency-exchange/getList",  HttpMethod.GET, null, new ParameterizedTypeReference<Object[]>() {
+          });
+		 
+			  Object[] response =  responseEntity.getBody();
+		  
+	    System.out.println("Rez:"+response.toString());
+	    return response;
+	    
+	    
+	  }
+
+}
---
+++ src/main/java/com/in28minutes/springboot/microservice/example/forex/SpringBootMicroserviceForexServiceApplication.java
@@ -0,0 +1,36 @@
+package com.in28minutes.springboot.microservice.example.forex;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Random;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.event.EventListener;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+@SpringBootApplication
+@EnableFeignClients("microservice.example.currencyconversion")
+public class SpringBootMicroserviceForexServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroserviceForexServiceApplication.class, args);
+	}
+	
+	
+	}
+
+
+
