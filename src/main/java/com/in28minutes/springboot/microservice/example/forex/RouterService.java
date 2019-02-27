72909d978ec8ae9672b230f5396368152963ce8b
nenadmudri <43782866+nenadmudri@users.noreply.github.com>
2019-02-27 10:17:07 +0100

    Update RouterService.java

--- src/main/java/com/in28minutes/springboot/microservice/example/forex/RouterService.java
+++ src/main/java/com/in28minutes/springboot/microservice/example/forex/RouterService.java
@@ -1,7 +1,10 @@
 package com.in28minutes.springboot.microservice.example.forex;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.ParameterizedTypeReference;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
 
 
 
